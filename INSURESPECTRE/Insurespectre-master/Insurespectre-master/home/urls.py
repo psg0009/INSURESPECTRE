9f26f155c5d3cfbb3702daaf1cf1641ac5b90455
psg0009 <139065268+psg0009@users.noreply.github.com>
2024-09-08 05:06:10 +0530

    UPDATE

---
+++ INSURESPECTRE/Insurespectre-master/Insurespectre-master/home/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('login', views.login_view, name='login'),
+    path('signup', views.signup_view, name='signup'),
+    path('logout/', views.logout_view, name='logout'),
+    path("team", views.team, name="team"),
+    path("contact", views.contact, name="contact"),
+    path('about/who/', views.who, name='who'),
+    path('about/what/', views.what, name='what'),
+    path('about/goal/', views.goal, name='goal'),
+    path('about', views.about, name='about'),
+    path('solution', views.solution, name='solution'),
+]
+
